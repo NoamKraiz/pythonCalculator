a503ad45c2de053d5350867a82933c154c617eef
NoamKraiz <nkraiz@gmail.com>
2023-11-24 11:46:24 +0200

    adding header

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 
+print("welcome to our python calculator")
+print("---------------------------------------")
+
 n1 = int(input("enter first number: "))
 n2 = int(input("enter second number: "))
 
 sum = n1+n2
-sub=n1-n2
-mul=n1*n2
-div=n1/n2
+sub = n1-n2
+mul = n1*n2
+div = n1/n2
 print(f"{n1}+{n2}={sum}")
 print(f"{n1}-{n2}={sub}")
 print(f"{n1}*{n2}={mul}")
